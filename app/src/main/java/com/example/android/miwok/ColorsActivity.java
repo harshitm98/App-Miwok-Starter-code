e1abcb3f93df4203a77a63fd8d649d34518ba665
harshitm98 <harshitmah98@gmail.com>
2017-06-29 20:25:08 +0530

    Added images to drawables

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,12 +1,8 @@
 package com.example.android.miwok;
 
-import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,7 +11,7 @@ public class ColorsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_number);
+        setContentView(R.layout.list_layout);
 
         ArrayList<Word> words = new ArrayList<Word>();
 
